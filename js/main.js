c185a0cd234af40d73a6f7f12c13e7647465495f
PaoloWD <paolo.lore.93@gmail.com>
2022-10-28 20:37:13 +0200

    add offert and remove console.log

--- js/main.js
+++ js/main.js
@@ -3,32 +3,25 @@ const submitBtn = document.querySelector(".btn-outline-primary");
 submitBtn.addEventListener("click", function () {
   const fullNameEl = document.querySelector(".full-name");
   const distanceEl = document.querySelector(".distance");
-  const distanza = parseInt(distanceEl.value);
+  const distanceCost = parseInt(distanceEl.value);
   const saleEl = document.querySelector(".sale");
-
-  let ticketPrice = distanza * 0.21;
+  const offert = document.querySelector(".offert");
+  let ticketPrice = distanceCost * 0.21;
   const subDistance = document.querySelector("span.distance");
   const subName = document.querySelector("span.full-name");
   const subSale = document.querySelector("span.sale");
-
-  console.log(fullNameEl);
-  console.log(distanceEl);
-  console.log(saleEl);
-  console.log(submitBtn);
-
   subName.innerHTML = fullNameEl.value;
-  console.log(fullNameEl.value);
-
   subDistance.innerHTML = distanceEl.value;
-  console.log(distanceEl.value);
-
   if (saleEl.value === "20") {
     ticketPrice = ticketPrice * 0.8;
     subSale.innerHTML = ticketPrice.toFixed(2);
+    offert.innerHTML = `20%`;
   } else if (saleEl.value === "40") {
     ticketPrice = ticketPrice * 0.6;
     subSale.innerHTML = ticketPrice.toFixed(2);
+    offert.innerHTML = `40%`;
   } else {
     subSale.innerHTML = ticketPrice.toFixed(2);
+    offert.innerHTML = `Non hai diritto a nessun sconto`;
   }
 });
