b0f0b076151fd2ff2f13e4ffce5e2d8a422ee40c
PaoloWD <paolo.lore.93@gmail.com>
2022-10-28 18:26:22 +0200

    add var inside the button, add parseInt, change toFixed

--- js/main.js
+++ js/main.js
@@ -1,32 +1,34 @@
-//validiamo i dati inseriti
-let ticketPriceSale;
-let fullNameEl = document.querySelector(".full-name");
-let distanceEl = document.querySelector(".distance");
-let saleEl = document.querySelector(".sale");
 const submitBtn = document.querySelector(".btn-outline-primary");
-const ticketPrice = (distanceEl * 0.21).toFixed(2);
-const subDistance = document.querySelector("span.distance");
-const subName = document.querySelector("span.full-name");
-const subSale = document.querySelector("span.sale");
-
-console.log(fullNameEl);
-console.log(distanceEl);
-console.log(saleEl);
-console.log(ticketPriceSale);
-console.log(submitBtn);
 
 submitBtn.addEventListener("click", function () {
+  const fullNameEl = document.querySelector(".full-name");
+  const distanceEl = document.querySelector(".distance");
+  const distanza = parseInt(distanceEl.value);
+  const saleEl = document.querySelector(".sale");
+
+  let ticketPrice = distanza * 0.21;
+  const subDistance = document.querySelector("span.distance");
+  const subName = document.querySelector("span.full-name");
+  const subSale = document.querySelector("span.sale");
+
+  console.log(fullNameEl);
+  console.log(distanceEl);
+  console.log(saleEl);
+  console.log(submitBtn);
+
   subName.innerHTML = fullNameEl.value;
   console.log(fullNameEl.value);
 
   subDistance.innerHTML = distanceEl.value;
   console.log(distanceEl.value);
 
-  if (saleEl.value === 20) {
+  if (saleEl.value === "20") {
     ticketPrice = ticketPrice * 0.8;
-    subSale.innerHTML = ticketPriceSale.value;
-  }
-  if (saleEl.value === 40) {
+    subSale.innerHTML = ticketPrice.toFixed(2);
+  } else if (saleEl.value === "40") {
     ticketPrice = ticketPrice * 0.6;
+    subSale.innerHTML = ticketPrice.toFixed(2);
+  } else {
+    subSale.innerHTML = ticketPrice.toFixed(2);
   }
 });
