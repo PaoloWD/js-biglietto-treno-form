445c60141afb6247647e8f4e28b4b06f72575b62
PaoloWD <paolo.lore.93@gmail.com>
2022-11-01 18:57:17 +0100

    add alert

--- js/main.js
+++ js/main.js
@@ -20,27 +20,32 @@ submitBtn.addEventListener("click", function () {
   const offert = document.querySelector(".offert");
   const randomNum = document.querySelector("span.random");
   const randomCarriage = document.querySelector("span.carriage");
-
-  let ticketPrice = distanceCost * 0.21;
-  const subName = document.querySelector("span.full-name");
-  const subSale = document.querySelector("span.sale");
-  subName.innerHTML = fullNameEl.value;
-  if (saleEl.value === "20") {
-    ticketPrice = ticketPrice * 0.8;
-    subSale.innerHTML = ticketPrice.toFixed(2);
-    offert.innerHTML = `20%`;
-  } else if (saleEl.value === "40") {
-    ticketPrice = ticketPrice * 0.6;
-    subSale.innerHTML = ticketPrice.toFixed(2);
-    offert.innerHTML = `40%`;
+  console.log(distanceEl.value);
+  if (distanceEl.value == "" || fullNameEl.value == "") {
+    alert("Compila tutti i campi");
   } else {
-    subSale.innerHTML = ticketPrice.toFixed(2);
-    offert.innerHTML = `0%`;
+    let ticketPrice = distanceCost * 0.21;
+    const subName = document.querySelector("span.full-name");
+    const subSale = document.querySelector("span.sale");
+    subName.innerHTML = fullNameEl.value;
+
+    if (saleEl.value === "20") {
+      ticketPrice = ticketPrice * 0.8;
+      subSale.innerHTML = ticketPrice.toFixed(2);
+      offert.innerHTML = `20%`;
+    } else if (saleEl.value === "40") {
+      ticketPrice = ticketPrice * 0.6;
+      subSale.innerHTML = ticketPrice.toFixed(2);
+      offert.innerHTML = `40%`;
+    } else {
+      subSale.innerHTML = ticketPrice.toFixed(2);
+      offert.innerHTML = `Non hai diritto ad alcuno sconto`;
+    }
+
+    const max10 = Math.ceil(Math.random() * 5);
+    randomNum.innerHTML = max10;
+
+    const max100000 = Math.ceil(Math.random() * 100000);
+    randomCarriage.innerHTML = max100000;
   }
-
-  const max10 = Math.ceil(Math.random() * 5);
-  randomNum.innerHTML = max10;
-
-  const max100000 = Math.ceil(Math.random() * 100000);
-  randomCarriage.innerHTML = max100000;
 });
